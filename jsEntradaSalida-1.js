e5f65297b761dc2cf337b7508ef816c387ed0dd9
hferreira140691 <hferreira@mecon.gob.ar>
2016-11-14 11:39:43 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,8 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+// la ventana (), muestra msj emergente
+alert("Esto funciona de maravilla");
+//literal es el msj de texto
+}
+
---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+ //Var crea variable
+ var nombre;
+ var apellido = " Ferreira";
+ nombre="Hernan ";
+nombre = prompt("ingrese su nombre", "NN");
+ alert("Mi nombre es: " + nombre +"Y su apellido es" + apellido);
+
+
+}
+
