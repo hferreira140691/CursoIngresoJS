99137b2636874fa4827987dfd702c6e613ee67be
hferreira140691 <hferreira@mecon.gob.ar>
2016-11-18 11:59:35 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,54 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+
+	PrecioUno= document.getElementById('PrecioUno').value;
+	PrecioDos= document.getElementById('PrecioDos').value;
+	PrecioTres= document.getElementById('PrecioTres').value;
+
+	PrecioUno= parseInt(PrecioUno);
+	PrecioDos= parseInt(PrecioDos);
+	PrecioTres= parseInt(PrecioTres);
+	alert(PrecioUno+PrecioDos+PrecioTres);
 }
 function Promedio () 
 {
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+	var total;
+
+	PrecioUno= document.getElementById('PrecioUno').value;
+	PrecioDos= document.getElementById('PrecioDos').value;
+	PrecioTres= document.getElementById('PrecioTres').value;
+
+	PrecioUno= parseInt(PrecioUno);
+	PrecioDos= parseInt(PrecioDos);
+	PrecioTres= parseInt(PrecioTres);
+	total= (PrecioUno+PrecioDos+PrecioTres)/3;
+	alert(total);
 	
 }
 function PrecioFinal () 
 {
-	
+	var PrecioUno;
+	var PrecioDos;
+	var PrecioTres;
+	var parcial
+	var total;
+
+
+	PrecioUno= document.getElementById('PrecioUno').value;
+	PrecioDos= document.getElementById('PrecioDos').value;
+	PrecioTres= document.getElementById('PrecioTres').value;
+
+	PrecioUno= parseInt(PrecioUno);
+	PrecioDos= parseInt(PrecioDos);
+	PrecioTres= parseInt(PrecioTres);
+	parcial=(PrecioUno+PrecioDos+PrecioTres);
+	total= (parcial+parcial*21/100);
+	alert(total);
 }
